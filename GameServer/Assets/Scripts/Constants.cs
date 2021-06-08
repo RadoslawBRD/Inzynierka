2b7e3414c5c7955b2bc4374876fa1bbba8226cf8
Radek Brd <radoslaw.brdek@gmail.com>
2021-06-08 20:07:00 +0200

    Ticks change

--- GameServer/Assets/Scripts/Constants.cs
+++ GameServer/Assets/Scripts/Constants.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Constants
 {
        
-        public const int TICKS_PER_SEC = 30;
+        public const int TICKS_PER_SEC = 60;
         public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
     
 }
