5e896aca3ae753ca1c230886267b348359ed02ba
RadoslawBRD <radoslaw.brdek@gmail.com>
2021-06-14 16:12:27 +0200

    Granade counter

---
+++ GameClient/Assets/Scripts/HUD/GranadeCount.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GranadeCount : MonoBehaviour
+{
+    public static GranadeCount instance;
+
+
+    public int currentGranade=0;
+    public Text granadeDisplay;
+
+
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Debug.Log("instance already exists, destroying object!");
+            Destroy(this);
+        }
+
+    }
+
+    public void setGranadeAmount(int _value)
+    {
+        if(currentGranade > 0 || _value > 0)
+            currentGranade += _value;
+        updateGranadeCount(currentGranade);
+    }
+    public void updateGranadeCount(int _count)
+    {
+        granadeDisplay.text = _count.ToString();
+
+    }
+}
